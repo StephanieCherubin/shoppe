163e51af98c8cff65db542acc5e58f128f2233a6
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-03-05 15:36:59 -0500

    Alter layouts and views to call out I18n module by way of the t() helper

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class AdminControllerTest < ActionDispatch::IntegrationTest
+class AdminControllerTest < ActionController::TestCase
   test "should get index" do
-    get admin_index_url
+    get :index
     assert_response :success
   end
 
